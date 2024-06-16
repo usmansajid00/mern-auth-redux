2053b79b240139a6b5b97db35b3e19f5e8b1adf6
usmansajid00 <usmansajid2517@gmail.com>
2024-06-16 23:16:18 +0500

    user model updated

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,18 +1,25 @@
 import mongoose from "mongoose";
-const { Schema } = mongoose;
 
-const UserSchema = new Schema(
+const userSchema = mongoose.Schema(
   {
-    name: { type: String, required: true },
-    username: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
   {
     timestamps: true,
   }
 );
 
-const User = mongoose.model("User", UserSchema, "users");
+const User = mongoose.model("User", userSchema, "users");
 
 export default User;
